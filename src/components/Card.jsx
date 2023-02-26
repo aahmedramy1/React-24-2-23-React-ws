7e395c6527784f13d9b8f145ddead7bc03214bd4
Ahmed Ramy <ahmedramy.e404@gmail.com>
2023-02-26 09:51:13 +0200

    Initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+const Card = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "gray",
+        color: "white",
+        borderRadius: "1rem",
+        padding: "1rem",
+      }}
+    >
+      <h1>card</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  );
+};
+
+export default Card;
